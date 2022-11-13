8b5be2b67d096178b38565e6d31b3c05f3bd0f58
Michal Cesarczyk <michal.cesarczyk@gmail.com>
2022-11-13 04:58:36 +0100

    created routing

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -1,9 +1,31 @@
-import React from 'react';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
+
+import * as ROUTES from '../../constants/routes';
+import Account from '../Account';
+import Admin from '../Admin';
+import Home from '../Home';
+import Landing from '../Landing';
+import Navigation from '../Navigation';
+import PasswordForget from '../PasswordForget';
+import SignIn from '../SignIn';
+import SignUp from '../SignUp';
 
 const App = () => (
-  <div>
-    <h1>App</h1>
-  </div>
+  <BrowserRouter>
+    <Navigation />
+
+    <hr />
+
+    <Routes>
+      <Route path={ROUTES.LANDING} element={<Landing />} />
+      <Route path={ROUTES.SIGN_UP} element={<SignUp />} />
+      <Route path={ROUTES.SIGN_IN} element={<SignIn />} />
+      <Route path={ROUTES.PASSWORD_FORGET} element={<PasswordForget />} />
+      <Route path={ROUTES.HOME} element={<Home />} />
+      <Route path={ROUTES.ACCOUNT} element={<Account />} />
+      <Route path={ROUTES.ADMIN} element={<Admin />} />
+    </Routes>
+  </BrowserRouter>
 );
 
 export default App;
